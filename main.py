e55ef435afd413b34e70f10dc4ec728f57c4f748
Rhys <2068188418@qq.com>
2023-01-04 15:57:15 +0800

    test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pytesseract
 from PIL import Image
 
+print("Test")
+
 # print(pytesseract.get_languages(config=''))
 # print(pytesseract.image_to_string(Image.open('test.jpg'), lang='chi_sim+eng'))
